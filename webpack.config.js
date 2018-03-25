99a86cdb1eb4c7713764a593fbcf94ce8fd4c683
kinjoBobchin <kinjo.bobchin@gmail.com>
2018-03-26 00:46:26 +0900

    babelの設定完了

--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,29 @@ module.exports = {
   devServer: {
     contentBase: "./dist",
     open: true
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子.jsの場合
+        test: /\.js$/,
+        use: [
+          {
+            // babelを使用する
+            loader: "babel-loader",
+            options: {
+              presets: [
+                // envを使用することでES2017 を ES5に変換
+                // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
+                // webpack の Tree Shaking 機能が使えない
+                ["env", {"modules": false}]
+              ]
+            }
+          }
+        ],
+        // node_moduleはbabelをかまさない
+        exclude: /node_modules/,
+      }
+    ]
   }
 }
